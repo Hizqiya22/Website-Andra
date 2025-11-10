bf35fddd97a7625dc7aa3729b0c12708607b7131
Hizqiya22 <hizqiyaheryanto2003@gmail.com>
2025-11-10 11:27:28 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>11240086 - Andra Asmara</title>
+<style>
+    body { font-family: Arial, sans-serif; margin: 0; }
+    .hilang { text-decoration: none; color: black; }
+    .hilang:hover { color: blue; text-decoration: underline; }
+    table { border-collapse: collapse; }
+    th { background-color: #e0e0e0; }
+    .konten { padding: 15px; }
+</style>
+</head>
+<body>
+
+<table width="100%" height="50%" border="1" cellspacing="0" cellpadding="0">
+<tr height="150">
+    <td colspan="2"><img src="image/header.png" height="150" width="100%" alt="Header" align='center' valign='center'></td>
+</tr>
+
+<tr>
+    <td width="200" valign="top">
+        <table width="100%" border="0" cellspacing="0" cellpadding="5">
+            <tr><th>MENU</th></tr>
+            <tr><td><img src="image/beranda.png" width="15" height="15"> <a href="index.php" class="hilang">Beranda</a></td></tr>
+            <tr><td><img src="image/profil.png" width="15" height="15"> <a href="profil.html" class="hilang">profil</a></td></tr>
+            <tr><td><img src="image/cv.png" width="15" height="15"> <a href="cv.html" class="hilang">cv</a></td></tr>
+            <tr><td><img src="image/galeri.png" width="15" height="15"> <a href="image.html" class="hilang">image</a></td></tr>
+           
+        </table>
+    </td>
+
+    <td valign="top" class="konten">
+        <h2>Selamat Datang!</h2>
+        <p>Halo, saya <b>andra asmara</b>. Ini adalah website pribadi saya yang berisi informasi tentang biodata, pendidikan, pelatihan, dan portofolio saya.</p>
+        
+        <p>Gunakan menu di sebelah kiri untuk menjelajahi halaman lainnya.</p>
+    </td>
+</tr>
+
+<tr height="30">
+    <td colspan="2"><img src="image/footer.png" height="150" width="100%" alt="Footer"></td>
+</tr>
+</table>
+
+</body>
+</html>
